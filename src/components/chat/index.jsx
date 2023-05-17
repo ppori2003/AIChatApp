33b70b3467276cb899c93c883ddbe11c8252cbe4
ppori2003 <sukbumhong97@gmail.com>
2023-05-17 14:34:53 +0900

    Created basic front end chat app

--- src/components/chat/index.jsx
+++ src/components/chat/index.jsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React from "react";
+import {
+  useMultiChatLogic,
+  MultiChatSocket,
+  MultiChatWindow,
+} from "react-chat-engine-advanced";
+import Header from "@/components/customHeader";
+import StandardMessageForm from "@/components/customMessageForms/StandardMessageForm";
 
-const Chat = () => {
-    return <div>Chat</div>;
+
+const Chat = ({ user, secret }) => {
+  const chatProps = useMultiChatLogic(
+    import.meta.env.VITE_PROJECT_ID,
+    //"f8c570b5-e6c9-4288-9603-a0bb20d080ec",
+    "testuser",
+    "1234"
+  );
+
+  return (
+    <div style={{ flexBasis: "100%" }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow
+        {...chatProps}
+        style={{ height: "100vh" }}
+        renderChatHeader={(chat) => <Header chat={chat} />}
+        renderMessageForm={(props) => {
+        
+          return (
+            <StandardMessageForm props={props} activeChat={chatProps.chat} />
+          );
+        }}
+      />
+    </div>
+  );
 };
 
 export default Chat;
